bb6612e9a53fa559b2897a25686b579009e78e16
Jinyoung <roghabo@gmail.com>
2024-07-04 14:36:33 +0900

    finish routing part

---
+++ components/navigation.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home{path === "/" ? " 🍎" : ""}</Link>
+        </li>
+        <li>
+          <Link href="/about-us">
+            About Us{path === "/about-us" ? " 🍎" : ""}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
